9ed682952bd33ca97a052edd75d0a7f0f8f7b9f2
Mru00-hub <mrudulabhalke75225@gmail.com>
2025-12-17 12:44:35 +0530

    Create healthcheck.js

---
+++ services/device-gateway/healthcheck.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+const options = {
+  host: 'localhost',
+  port: process.env.PORT || 8082,
+  path: '/health',
+  timeout: 2000,
+};
+
+const healthCheck = http.request(options, (res) => {
+  if (res.statusCode === 200) {
+    process.exit(0);
+  } else {
+    process.exit(1);
+  }
+});
+
+healthCheck.on('error', () => {
+  process.exit(1);
+});
+
+healthCheck.end();
