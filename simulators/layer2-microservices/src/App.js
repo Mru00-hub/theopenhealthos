385e49a3e909793f711aca7750067eaf9db3c16b
Mru00-hub <mrudulabhalke75225@gmail.com>
2025-12-17 13:33:38 +0530

    Create App.js

---
+++ simulators/layer2-microservices/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MicroservicesSimulator from './components/MicroservicesSimulator';
+import './index.css'; // This will hold your Tailwind imports
+
+function App() {
+  return (
+    <div className="App">
+      <MicroservicesSimulator />
+    </div>
+  );
+}
+
+export default App;
