cad3879c9dd03eacc7000b2b1b6f576f70e8c61b
Mru00-hub <mrudulabhalke75225@gmail.com>
2025-12-20 08:55:20 +0530

    Update index.js

--- simulators/layer2-microservices/src/index.js
+++ simulators/layer2-microservices/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import eruda from 'eruda';
+eruda.init();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
